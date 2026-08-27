ff23da917e38ce9abcc7949c8f5ce664ed25eb55
2046945334-oss <2046945334@qq.com>
2026-08-27 10:09:35 +0800

    fix: force production mode in server.js to disable dev error overlay

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
 
-const dev = process.env.NODE_ENV !== 'production'
+const dev = false // Always run in production mode
 const port = parseInt(process.env.PORT, 10) || 3000
 const app = next({ dev })
 const handle = app.getRequestHandler()
